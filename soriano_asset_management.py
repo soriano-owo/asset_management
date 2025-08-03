3bb810a3fdbd60524372dc5989e47a0a0f906dd3
Adrian Fuentes Soriano <adrian.fuentes.sor@ciencias.unam.mx>
2025-08-02 23:53:23 -0600

    Nueva versión

--- soriano_asset_management.py
+++ soriano_asset_management.py
@@ -82,7 +82,7 @@ if ticker:
         font=dict(color="white")
     ),
     title=dict(
-        text=f"Close price of {ticker}",
+        text=f"Close of {ticker}",
         font=dict(color="white")
     )
     
